a5d5112b843d7d56b6194c8477d5ca22bd7edb85
shchemelinin <shchemelinin@ya.ru>
2023-02-18 21:00:49 +0300

    Добавлено логгирование v0.5

--- main.py
+++ main.py
@@ -4,5 +4,9 @@ pip.main(['install', 'cryptography'])
 from background import keep_alive  # импорт функции для поддержки работоспособности
 keep_alive()  # запускаем flask-сервер в отдельном потоке.
 
+import logging as log
+log.basicConfig(filename='halkon.log', format='%(asctime)s %(levelname)s %(message)s',
+                encoding='utf-8', level=log.INFO)
+
 #import dataMerge
 import halkonBot
