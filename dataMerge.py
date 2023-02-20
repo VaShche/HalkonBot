dc34acdbbd8548e57022b330ac50347c1639ed77
shchemelinin <shchemelinin@ya.ru>
2023-02-20 14:12:01 +0300

    добавление в структуру соседей за стенкой fix v0.5

--- dataMerge.py
+++ dataMerge.py
@@ -16,8 +16,8 @@ house_data = func.load_dict_from_file(conf['BOT']['data'], key=key)
 l1 = [42, 44, 46, 48, 50, 52, 54, 43, 45, 47, 49, 51, 53, 55, 57]
 l2 = [23, 26, 29, 32, 35, 38, 41, 58, 59, 61, 63, 65, 67, 69, 71]
 for i, n in enumerate(l1):
-    flats.Flat.findByFlatID(flats.getAllHouseFlats(house_data), n).wall_residents.append(l2[i])
-    flats.Flat.findByFlatID(flats.getAllHouseFlats(house_data), l2[i]).wall_residents.append(n)
+    flats.Flat.findByFlatID(flats.getAllHouseFlats(house_data), n).wall_residents = [l2[i]]
+    flats.Flat.findByFlatID(flats.getAllHouseFlats(house_data), l2[i]).wall_residents = [n]
 func.save_dict_to_file(conf['BOT']['data'], house_data, key=conf['BOT']['cryptokey'])
 
 print(house_data)
--- flats.py
+++ flats.py
@@ -214,8 +214,8 @@ def getHalkonFlatsStruct():
     l1 = [42, 44, 46, 48, 50, 52, 54, 43, 45, 47, 49, 51, 53, 55, 57]
     l2 = [23, 26, 29, 32, 35, 38, 41, 58, 59, 61, 63, 65, 67, 69, 71]
     for i, n in enumerate(l1):
-        Flat.findByFlatID(getAllHouseFlats(halkon_flats), n).wall_residents.append(l2[i])
-        Flat.findByFlatID(getAllHouseFlats(halkon_flats), l2[i]).wall_residents.append(n)
+        Flat.findByFlatID(getAllHouseFlats(halkon_flats), n).wall_residents = [l2[i]]
+        Flat.findByFlatID(getAllHouseFlats(halkon_flats), l2[i]).wall_residents = [n]
     '''коммерция'''
     entrance = COMMERCE
     halkon_flats[entrance] = [Flat(None, entrance, 1)]
